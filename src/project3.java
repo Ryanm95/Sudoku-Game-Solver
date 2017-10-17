7f935516a610a3743fe14b2839b048f6bc3cb004
Edgar <emart9@uic.edu>
2017-10-17 16:20:16 -0500

    commented gui

--- src/project3.java
+++ src/project3.java
@@ -3,7 +3,7 @@ public class project3 {
     public static void main(String[] args){
 
         // read in text file and update board
-        //GUI gui = new GUI();
+        GUI gui = new GUI();
         //System.out.println("hello world");
     }
 }
