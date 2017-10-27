ef760594f9887461d556f00927a72cc7eca7335a
Rmoran95 <rmoran8@uic.edu>
2017-10-27 12:25:48 -0500

    Added solve class

---
+++ src/Solve.java
@@ -0,0 +1,31 @@
+public class Solve {
+
+    //
+    // Solves puzzle using 4 algorithms
+    //
+    public void solvePuzzle(){
+        // TODO: While loop looping through algorithms
+        // each while loop check for a win
+    }
+
+    //
+    // Checks sudoku board for a win
+    //
+    private void checkForWin(){
+        checkRows();
+        checkCols();
+        checkGrid();
+    }
+
+    private boolean checkRows(){
+        return true;
+    }
+
+    private boolean checkCols(){
+        return true;
+    }
+
+    private boolean checkGrid(){
+        return true;
+    }
+}
